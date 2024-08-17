d234f8487be7f44eb4ff10056ee9f91bb7005da8
IsmayilovaKutaisi <kutaisni@code.edu.az>
2024-08-18 02:20:45 +0400

    file created

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Homework1708;
+
+Console.WriteLine("İstifadəçi adını daxil edin:");
+string username = Console.ReadLine();
+Console.WriteLine("Şifrəni daxil edin:");
+string password = Console.ReadLine();
+
+User userObject = new User(username, password);
+Console.WriteLine($"Hesab yaradıldı:UserName={userObject.UserName},Password={userObject.Password}");
+
+
---
+++ User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Homework1708
+{
+	public class User
+	{
+		public string UserName { get; private set; }
+		public string Password { get; private set; }
+
+		public User(string username,string password)
+		{
+			if(username.Length<6||username.Length>25)
+			{
+				Console.WriteLine("İstifadəçi adı minimum 6,maxsimum 25 simvoldan ibarət olmalıdır.");
+			}
+			else
+			{
+				this.UserName = username;
+			}
+			if(password.Length<8||password.Length>25||!ContainsUpperCase(password)||!ContainsLowerCase(password)||!ContainsDigit(password))
+			{
+				Console.WriteLine("şifrə minimum 8,maxsimum 25 simvoldan ibarət olmalıdır və ən azı bir böyük hərf,bir kiçik hərf,bir rəqəmdən istifadə olunmalıdır.");
+			}
+			else
+			{
+				this.Password = password;
+			}
+		}
+		public bool HasDigit()
+		{
+			return
+				ContainsDigit(this.Password);
+		}
+		public bool HasUpper()
+		{
+			return ContainsUpperCase(this.Password);
+		}
+		public bool HasLower()
+		{
+			return ContainsLowerCase(this.Password);
+		}
+		private bool ContainsDigit(string value)
+		{
+			return value.Any(char.IsDigit);
+		}
+		private bool ContainsUpperCase(string value)
+		{
+			return value.Any(char.IsUpper);
+		}
+		private bool ContainsLowerCase(string value)
+		{
+            return value.Any(char.IsLower);
+		}
+
+	}
+}
+
